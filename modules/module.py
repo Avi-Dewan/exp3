c4be464a8c0f060c447a8133f06600fc838b5007
ShinraTenseiRe <avidewan05@gmail.com>
2024-09-13 12:21:31 +0600

    bhjjh

--- modules/module.py
+++ modules/module.py
@@ -7,13 +7,6 @@ def feat2prob(feat, center, alpha=1.0):
     q = (q.t() / torch.sum(q, 1)).t()
     return q
 
-def target_distribution(q, epsilon=1e-6):
-    # Add a small value epsilon to avoid division by zero
-    weight = q**2 / (q.sum(0) + epsilon)
-    
-    # Ensure no division by zero during transpose
-    transposed_weight = weight.t()
-    normalized_weight = transposed_weight / (transposed_weight.sum(1, keepdim=True) + epsilon)
-    
-    return normalized_weight.t()
-
+def target_distribution(q):
+    weight = q**2 / q.sum(0)
+    return (weight.t() / weight.sum(1)).t()
